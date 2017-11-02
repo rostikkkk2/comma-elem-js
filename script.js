8e01c4312f1bbb6032a5d4509d26f96fef4e6419
Rostik Yuriev <rost4402255@gmail.com>
2017-11-02 22:02:07 +0200

    added var last_el

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ var arr = ["cot", "rost", "post", "slon"];
 var i = 0;
 var elems = "";
 var final_res = "";
+var last_el = arr[arr.length -1];
 
 while (i < arr.length) {
-  if (arr[i] != arr[arr.length -1]) {
+  if (arr[i] != last_el) {
     elems += arr[i] + " , ";
   }else{
-    final_res = elems + arr[arr.length -1];
+    final_res = elems + last_el;
   }
   i++;
 }
