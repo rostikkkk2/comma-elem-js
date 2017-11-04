2b8a9050ee0e09fc7fc667d21436e4003fad2e97
Rostik Yuriev <rost4402255@gmail.com>
2017-11-04 14:08:14 +0200

    delete var final

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var arr = ["cot", "rost", "post", "slon"];
 var i = 0;
 var elems = "";
-var final_res = "";
 
 while (i < arr.length) {
   if (arr[i] != arr[arr.length -1]) {
     elems += arr[i] + " , ";
   }else{
-    final_res = elems + arr[arr.length -1];
+    elems = elems + arr[arr.length -1];
   }
   i++;
 }
 
-alert(final_res);
+alert(elems);
